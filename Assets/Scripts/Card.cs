24fd9d81b76f56472bcc341483d72b3f2737aa67
hatulmadan1 <katiq@inbox.ru>
2019-11-16 18:59:30 +0300

    Cards added

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public abstract class GameCard
+{
+    private XmlDocument xCard = new XmlDocument();
+    protected XmlNode curCard;
+    public string ID = "01111";
+
+
+    public int Type { get; private set; }
+
+    public Sprite Sprite1;
+    public Sprite Sprite2;
+    public string CardText { get; private set; }
+    public string AcceptText { get; private set; }
+    public string DeclineText { get; private set; }
+
+    public GameCard(string id)
+    {
+        ID = id;
+        xCard.Load("./CardCollection.xml");
+        XmlElement xRoot = xCard.DocumentElement;
+        string xPath = "card[@id='" + ID + "']";
+        curCard = xRoot.SelectSingleNode(xPath);
+
+        CardText = curCard.SelectSingleNode("text").InnerText;
+        AcceptText = curCard.SelectSingleNode("accept").InnerText;
+        DeclineText = curCard.SelectSingleNode("decline").InnerText;
+
+        switch (curCard.SelectSingleNode("type").InnerText)
+        {
+            case "green":
+                Type = 0;
+                break;
+            case "orange":
+                Type = 1;
+                break;
+            case "red":
+                Type = 2;
+                break;
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/UsualGameCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class UsualGameCard : GameCard
+{
+    public void Accept(GameController g)
+    {
+        int[] p = {System.Int32.Parse(curCard.SelectSingleNode("accept").Attributes.GetNamedItem("p1").Value),
+            System.Int32.Parse(curCard.SelectSingleNode("accept").Attributes.GetNamedItem("p2").Value),
+            System.Int32.Parse(curCard.SelectSingleNode("accept").Attributes.GetNamedItem("p3").Value) };
+        g.Anonymity += p[0];
+        g.Money += p[1];
+        g.Feelings += p[2];
+    }
+
+    public void Decline(GameController g)
+    {
+        int[] p = {System.Int32.Parse(curCard.SelectSingleNode("decline").Attributes.GetNamedItem("p1").Value),
+            System.Int32.Parse(curCard.SelectSingleNode("decline").Attributes.GetNamedItem("p2").Value),
+            System.Int32.Parse(curCard.SelectSingleNode("decline").Attributes.GetNamedItem("p3").Value) };
+        g.Anonymity += p[0];
+        g.Money += p[1];
+        g.Feelings += p[2];
+    }
+}
