774b91d29a9489e0c78ff8689e05879843dde5cd
Илья <xxxilayxxx@gmail.com>
2019-11-17 13:45:07 +0300

    Shooting mechanic and health added

---
+++ Assets/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public Sprite RIP;
+    public int Health;
+    void ApplyDamage(int Damage)
+    {
+        Health -= Damage;
+        if (Health <= 0)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = RIP;
+            if (!GetComponent<Rigidbody2D>())
+            {
+                gameObject.AddComponent<Rigidbody2D>();
+            }
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * 100);
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+            GetComponent<Rigidbody2D>().mass = 10;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Weapon weapon;
+    private void Update()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        Vector3 dir = (pos - transform.position).normalized;
+        
+        weapon.transform.rotation = Quaternion.FromToRotation(Vector3.right, dir); 
+    }
+    void FixedUpdate()
+    {
+        weapon.transform.position = transform.position + Vector3.up * 0.5f;
+
+        if (Input.GetMouseButton(0))
+        {
+            weapon.Shoot();
+        }
+    }
+}
