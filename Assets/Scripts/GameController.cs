a1ae1ceac08cac7484ba0c0d563d4967107f861e
hatulmadan1 <katiq@inbox.ru>
2019-11-17 20:14:21 +0300

    It works

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -29,43 +29,47 @@ public class GameController : MonoBehaviour
         Anonymity = 100;
         Money = 100;
         Feelings = 100;
-        refresh();
     }
 
     private CardGen g = new CardGen();
     public GameCard curCard;
-
+    public bool isDangOrDeathCard = false;
     XmlDocument d = new XmlDocument();
 
 
     public void Start()
     {
+        refresh();
+        d.Load("Assets/Scripts/CardCollection.xml");
+        flags = new bool[d.DocumentElement.ChildNodes.Count];
         newCard();
 
-        d.Load("Assets/Scripts/CardCollection.xml");
-
-        flags = new bool[d.DocumentElement.ChildNodes.Count];
-
         xDoc.Load("Assets/Scripts/StatSaver.xml");
-        
+
+        xDoc.Save("Assets/Scripts/StatSaver.xml");
+
         aPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText;
         mPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("money").InnerText;
         fPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("feelings").InnerText;
         
     }
 
-    public void Acc ()
+    public void Acc()
     {
+        
         curCard.SceneContr = sc;
         curCard.numberOfDungeons = NumberOfDungeons;
         curCard.Accept(this);
-
         
+        xDoc.Load("Assets/Scripts/StatSaver.xml");
+        xDoc.DocumentElement.SelectSingleNode("flags").InnerText = boolToString(this.flags);
+        xDoc.Save("Assets/Scripts/StatSaver.xml");
 
         xDoc.Load("Assets/Scripts/StatSaver.xml");
         aPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText;
         mPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("money").InnerText;
         fPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("feelings").InnerText;
+        
         if (xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText == "0")
         {
             Death("10");
@@ -78,10 +82,10 @@ public class GameController : MonoBehaviour
         {
             Death("12");
         }
-        else newCard();
+        else if(!isDangOrDeathCard) newCard();
     }
 
-    public void Dec ()
+    public void Dec()
     {
         curCard.SceneContr = sc;
         curCard.numberOfDungeons = NumberOfDungeons;
@@ -89,9 +93,11 @@ public class GameController : MonoBehaviour
 
         xDoc.Load("Assets/Scripts/StatSaver.xml");
 
+
         aPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText;
         mPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("money").InnerText;
         fPannel.GetComponent<Text>().text = xDoc.DocumentElement.SelectSingleNode("feelings").InnerText;
+
         if (xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText == "0")
         {
             Death("10");
@@ -104,44 +110,78 @@ public class GameController : MonoBehaviour
         {
             Death("12");
         }
-        else newCard();
+        else if(!isDangOrDeathCard) newCard();
     }
 
     public void newCard()
     {
-        curCard = g.getNextCard();
+        xDoc.Load("Assets/Scripts/StatSaver.xml");
+        if (xDoc.DocumentElement.SelectSingleNode("flagssaved").InnerText == "1")
+        {
+            string s = xDoc.DocumentElement.SelectSingleNode("flags").InnerText;
+            for (int i = 0; i < s.Length; i++)
+            {
+                flags[i] = (s[i] == '1');
+            }
+            xDoc.DocumentElement.SelectSingleNode("flagssaved").InnerText = "0";
+            xDoc.DocumentElement.SelectSingleNode("flags").InnerText = "";
+
+            
+            xDoc.Load("Assets/Scripts/StatSaver.xml");
+        }
         Anonymity = System.Int32.Parse(xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText);
         Money = System.Int32.Parse(xDoc.DocumentElement.SelectSingleNode("money").InnerText);
         Feelings = System.Int32.Parse(xDoc.DocumentElement.SelectSingleNode("feelings").InnerText);
+        isDangOrDeathCard = false;
+        curCard = g.getNextCard();
         tRefr();
-        string hhh = "";
-        for (int i = 0; i < flags.Length; i++)
-        {
-            hhh += flags[i].ToString();
-        }
-        Debug.Log(hhh);
     }
 
     public void Death(string ind)
     {
+        isDangOrDeathCard = true;
         curCard = new DungeGameCard(ind, "DeathCards", 2, 2);
         tRefr();
-        refresh();
+        xDoc.Load("Assets/Scripts/StatSaver.xml");
+
+        xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText = "100";
+        xDoc.DocumentElement.SelectSingleNode("money").InnerText = "100";
+        xDoc.DocumentElement.SelectSingleNode("feelings").InnerText = "100";
+
+        Debug.Log(xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText);
+
+        xDoc.Save("Assets/Scripts/StatSaver.xml");
     }
 
     void tRefr()
     {
+        //Debug.Log("Log");
         tCard.GetComponent<Text>().text = curCard.CardText;
         tAcc.GetComponent<Text>().text = curCard.AcceptText;
         tDec.GetComponent<Text>().text = curCard.DeclineText;
     }
     void refresh()
     {
+        //if (isDangOrDeathCard) return;
         xDoc.Load("Assets/Scripts/StatSaver.xml");
-        xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText = "100";
-        xDoc.DocumentElement.SelectSingleNode("money").InnerText = "100";
-        xDoc.DocumentElement.SelectSingleNode("feelings").InnerText = "100";
-        Debug.Log(xDoc.DocumentElement.SelectSingleNode("feelings").InnerText);
+        if (xDoc.DocumentElement.SelectSingleNode("flagssaved").InnerText == "0")
+        {
+            xDoc.DocumentElement.SelectSingleNode("anonymity").InnerText = "100";
+            xDoc.DocumentElement.SelectSingleNode("money").InnerText = "100";
+            xDoc.DocumentElement.SelectSingleNode("feelings").InnerText = "100";
+        }
+        xDoc.DocumentElement.SelectSingleNode("flagssaved").InnerText = "0";
         xDoc.Save("Assets/Scripts/StatSaver.xml");
+    }
+
+    string boolToString(bool[] f)
+    {
+        string ans = "";
+        for (int i = 0; i < f.Length; i++)
+        {
+            if (f[i]) ans += "1";
+            else ans += "0";
+        }
+        return ans;
     }
 }
